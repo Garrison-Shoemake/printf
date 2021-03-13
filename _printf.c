21cc43334d3d5bf88a955b31a9234e47b7b75333
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-13 21:31:36 +0000

    add first draft of printf, update header

--- _printf.c
+++ _printf.c
@@ -3,6 +3,50 @@
  *
  *
  */
-void _printf();
+
+
+
+//helper here//
+
+
+
+int _printf(const char *format, ...)
 {
+	va_list input;
+	int count = 0;
+	unsigned int i = 0;
+	unsigned int j;
+	dt dtstruct[] =
+	{
+		{"d", printi},
+		{"i", printi},
+		{"s", prints},
+		{"c", printc},
+		{"n", printn},
+		{NULL, NULL},
+	}
+
+	va_start(input, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == "%" || format[i] == "\\")
+		{
+			j = 0;
+			while (dtstruct[j].specifier)
+			{
+				//("%c\n", 'H')
+				if(dtstruct[j].specificer[0] == format[i+1])
+				{
+					count = dtstruct[j].formtype(input, count);
+				}
+			}
+			j++;
+		}
+		_putchar(input[i]);
+		count++;
+		i++;
+	}
+	va_end(input);
+	return(count);
 }
--- holberton.h
+++ holberton.h
@@ -2,5 +2,15 @@
 #define PRINTF_H
 
 int _putchar(char c);
+int _printf(const char *format, ...);
+
+/**
+ */
+typedef struct DataType
+{
+	char *specifier;
+	void (*formtype)(va_list );
+
+} dt;
 
 #endif
