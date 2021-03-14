e456ab86981857bd445f2dfa819094e3f55c43e2
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-03-14 01:08:44 +0000

    edited functions to account for count, need to fuse d and i funcs

--- functions.c
+++ functions.c
@@ -3,6 +3,35 @@
  *
  *
  */
-void dothis();
+void printd()
 {
+
 }
+/**
+*/
+void printi()
+{
+
+}
+/**
+ */
+void printc()
+{
+
+}
+/**
+ */
+int prints(va_list input, int count)
+{
+	char *c = va_arg(input, char *);
+	while (c != NULL)
+	{
+		_putchar(c[i]);
+		count++;
+		i++;
+	}
+	return (count);
+}
+
+
+//"%c/n"
